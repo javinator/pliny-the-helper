83dab8a13935d474b9bd6928bbd6602ee5fe2ab7
Vince Hofer <mail@vincehofer.ch>
2023-04-12 00:37:42 +0200

    Laid the base for the recipe editor

---
+++ src/app/pages/edit-recipe/edit-details/edit-details.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IonicModule} from '@ionic/angular';
+import {Recipe} from "../../../models/recipe.model";
+import {StorageService} from "../../../services/storage.service";
+
+@Component({
+  selector: 'edit-details-card',
+  templateUrl: 'edit-details.component.html',
+  styleUrls: ['../../../app.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class EditDetailsComponent {
+  constructor(private storage: StorageService) {
+  }
+
+  @Input()
+  recipe!: Recipe;
+
+  changeName(event: any) {
+    this.recipe.name = event.target.value;
+    this.storage.saveRecipe(this.recipe);
+  }
+}
