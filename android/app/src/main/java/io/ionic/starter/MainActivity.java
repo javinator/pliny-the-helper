50f9b70249f0db8c730e8eace9942db44f663d29
Vince Hofer <mail@vincehofer.ch>
2023-04-23 23:13:05 +0200

    android stuff

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.bierfreunde.pliny;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.bierfreunde.pliny',
   appName: 'Pliny the Helper',
   webDir: 'www',
   bundledWebRuntime: false
